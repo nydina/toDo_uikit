729d48928389f106c4e9bb4c0685f7facf32b86a
Razafindratsira Ny Dina <nydina.razafindratsira@legrand.com>
2024-02-17 16:48:55 +0100

    Add accessibility identifier to done button

--- ToDo_UIKit/TaskListViewController/TaskListViewController+DataSource.swift
+++ ToDo_UIKit/TaskListViewController/TaskListViewController+DataSource.swift
@@ -4,6 +4,14 @@ extension TaskListViewController {
     typealias DataSource = UICollectionViewDiffableDataSource<Int, Record>
     typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Record>
 
+    var recordCompletedValue: String {
+         NSLocalizedString("Completed", comment: "Record completed value")
+     }
+     var recordNotCompletedValue: String {
+         NSLocalizedString("Not completed", comment: "Record not completed value")
+     }
+
+
     func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, record: Record) {
         // Access the TaskItem from the Record
         let taskItem = record.fields
@@ -17,18 +25,39 @@ extension TaskListViewController {
 
         var doneButtonConfiguration = doneButtonConfiguration(for: record)
         doneButtonConfiguration.tintColor = .systemPurple
-
+       
+        cell.accessibilityCustomActions = [doneButtonAccessibilityAction(for: record)]
+        cell.accessibilityValue =
+        record.fields.isComplete ?? false ? recordCompletedValue : recordNotCompletedValue
         cell.accessories = [
             .customView(configuration: doneButtonConfiguration), .disclosureIndicator(displayed: .always)
         ]
     }
+
+    private func doneButtonAccessibilityAction(for record: Record) -> UIAccessibilityCustomAction
+       {
+           let name = NSLocalizedString(
+               "Toggle completion", comment: "Record done button accessibility label")
+           let action = UIAccessibilityCustomAction(name: name) { [weak self] action in
+               Task {
+                   do {
+                       try await self?.presenter.completeTask(record)
+
+                   } catch {
+
+                   }
+               }
+               return true
+           }
+           return action
+       }
 
     private func doneButtonConfiguration(
         for record: Record) -> UICellAccessory.CustomViewConfiguration {
             let symbolName = record.fields.isComplete ?? false ? "checkmark.circle.fill" : "circle"
             let symbolConfiguration = UIImage.SymbolConfiguration(textStyle: .title1)
             let image = UIImage(systemName: symbolName, withConfiguration: symbolConfiguration)
-            
+
             let button = UIButton()
             button.setImage(image, for: .normal)
 
